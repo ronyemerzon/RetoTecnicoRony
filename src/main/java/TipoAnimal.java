6afd623beb6624b3bcc5706e5c636ed057019486
ronyemerzon <https://github.com/ronyemerzon/fe-ct-prepcourse-fs.git>
2025-02-06 10:37:19 -0500

    Enum TipoAnimal mejorado

--- src/main/java/TipoAnimal.java
+++ src/main/java/TipoAnimal.java
@@ -1,7 +1,23 @@
 enum TipoAnimal {
 
-    TERRESTRE,
-    VOLADOR,
-    ACUATICO;
+    TERRESTRE("Terrestre", "Se desplaza por tierra."),
+    VOLADOR("Volador", "Se desplaza por los aires, puede volar."),
+    ACUATICO("Acuatico", "Su hábitad es el mar, puede nadar.");
+
+    final String nombre;
+    final String descripcion;
+
+    TipoAnimal(String nombre, String descripcion){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getTipo(){
+        return descripcion;
+    }
 
 }
