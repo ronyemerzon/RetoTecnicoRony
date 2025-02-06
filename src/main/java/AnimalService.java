06a35e69b658e400acae395d199abc901e574a25
ronyemerzon <https://github.com/ronyemerzon/fe-ct-prepcourse-fs.git>
2025-02-06 14:53:47 -0500

    Junit implementado

--- src/main/java/AnimalService.java
+++ src/main/java/AnimalService.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class AnimalService {
-    public static void agruparAnimales(List<Animal> animales){
+
+    private List<Animal> listaAnimales;
+
+    public AnimalService(){
+        listaAnimales = new ArrayList<>();
+    }
+
+    public void agregarAnimal(Animal animal){
+        listaAnimales.add(animal);
+    }
+
+    public List<Animal> getListaAnimales(){
+        return listaAnimales;
+    }
+
+    public void agruparAnimales(List<Animal> animales){
         Map<TipoAnimal, List<Animal>> agrupados = animales.stream().collect(Collectors.groupingBy(Animal::getTipoAnimal));
 
         agrupados.forEach((tipo, lista) -> {
